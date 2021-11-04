eae61fb9c4109a731b45f19dd37b8ec30a0d8108
Clarke Verdel <info@claver-webdesign.nl>
2021-11-04 21:59:47 +0100

    docs(): Add inline comment as a todo

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -4,6 +4,11 @@ interface IMarquee {
     children: React.ReactNode
 }
 
+// TODO: Create intersection observer for Marquee
+// Once the first element moves off the screen, append it in the list. Creating infinite loop
+// CSS Marquee transition blinks when it resets, that's what we want to avoid
+// GOAL: Create a generic marquee element that relies on CSS animation and only relies on Javascript to orchestrate it
+
 const Marquee = ( { children }: IMarquee ) => {
 
     const { ref, inView }  = useInView({
